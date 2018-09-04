cb03a62b8da8792048de8efb0d027c9dc3f3e6d4
Evnica <evnica@gmail.com>
2018-09-04 11:49:30 +0200

    Initial commit

---
+++ src/com/evnica/main/Endomain.java
@@ -0,0 +1,12 @@
+package com.evnica.main;
+
+/**
+ * Class: Endomain
+ * Version: 0.1
+ * Created on 28.08.2018 with the help of IntelliJ IDEA (thanks!)
+ * Author: Evnica
+ * Description:
+ */
+public class Endomain
+{
+}
---
+++ src/com/evnica/main/model/Point.java
@@ -0,0 +1,12 @@
+package com.evnica.main.model;
+
+/**
+ * Class: Point
+ * Version: 0.1
+ * Created on 17.08.2018 with the help of IntelliJ IDEA (thanks!)
+ * Author: Evnica
+ * Description:
+ */
+public class Point
+{
+}
